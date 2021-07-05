f96f50adf61d3dbfce5c49a93bcb8f9724925a17
Lukasz Przezdziek <l.przezdziek@gmail.com>
2021-07-05 22:06:20 +0200

    Adapter

---
+++ strukturalne/adapter.php
@@ -0,0 +1,36 @@
+<?php 
+
+/**
+ * Adapter/Wrapper -> wzorzec zapewnie współpracę między obiektami o niezgodnych interfejsach.
+ */
+
+
+interface File
+{
+    public function loadXmlFile();
+}
+
+class XML implements File{
+
+    public function loadXmlFile()
+    {
+    
+        //old version
+        //echo 'Loading xml from file...';
+
+        //add new implementation
+        $json = new JSON();
+        $json->loadJsonFromFile();
+    }
+
+}
+
+class JSON
+{
+    public function loadJsonFromFile(){
+        echo 'Loading json data from file...';
+    }
+}
+
+$new_xml_obj = new XML();
+$new_xml_obj->loadXmlFile();
